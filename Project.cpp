6ea990ffb28b81e6301882c3917be6c8be270712
OmniBro <jake.mitchell@colorado.edu>
2015-05-02 15:50:33 -0600

    Added unbuilt dictionary checking

--- Project.cpp
+++ Project.cpp
@@ -13,7 +13,7 @@
 
 //using namespace std;
 
-//void readFile(RBTree* Tree, WordList* List, HashTable* Table); 
+//void readFile(RBTree* Tree, WordList* List, HashTable* Table);
 
 
 
@@ -30,6 +30,9 @@ string input2;
 int command=-1;
 int command2 = -1;
 int command3 = -1;
+bool rbTreebuilt = false;
+bool listBuilt = false;
+bool hashBuilt = false;
 while (command !=6)
 {
 	command=-1;
@@ -45,10 +48,10 @@ switch (command)
 {
 case 1: //build dictionary
 {
-	command2=-1;	
+	command2=-1;
 	while (command2 != 5) //switch case for build dictionary submenu
 	{
-	
+
 	printBuildMenu();
 	cin>>ws;
 	getline(cin,input,'\n');
@@ -64,6 +67,7 @@ case 1: //build dictionary
 
 		readFile(&RedBlackTree, &LinkedList, &Table, 1);
 		command2=5;
+		rbTreebuilt = true;
 		break;
 	}
 	case 2: //build hash table
@@ -71,11 +75,12 @@ case 1: //build dictionary
 		//HashTable Table;
 		readFile(&RedBlackTree, &LinkedList, &Table, 2);
 		command2=5;
+		hashBuilt = true;
 		break;
 	}
 	case 3: //build linked list
 	{
-		bestOrWorstCaseLinkedList(); 
+		bestOrWorstCaseLinkedList();
 		cin>>ws;
 		getline(cin,input,'\n');
 		try
@@ -89,6 +94,7 @@ case 1: //build dictionary
 				LinkedList.resetOpCount();
 				readFile(&RedBlackTree, &LinkedList, &Table, 3);
 				command2=5;
+				listBuilt = true;
 				break;}
 		case 2:
 			{	LinkedList.emptyList();
@@ -96,6 +102,7 @@ case 1: //build dictionary
 				LinkedList.resetOpCount();
 				readFile(&RedBlackTree, &LinkedList, &Table, 4);
 				command2=5;
+				listBuilt = true;
 				break;}
 
 
@@ -108,7 +115,7 @@ case 1: //build dictionary
 	}
 	case 4: //build all
 	{	RedBlackTree.emptyTree(); //empty dictionaries to prevent problems/conflicts/reset to default
-		Table.emptyHashTable(); 
+		Table.emptyHashTable();
 		LinkedList.emptyList();
 
 
@@ -119,6 +126,9 @@ case 1: //build dictionary
 
 
 		command2=5;
+		rbTreebuilt = true;
+		listBuilt = true;
+		hashBuilt = true;
 		break;
 	}
 	default:
@@ -140,46 +150,68 @@ case 2: //search for a word
 	cout << "Enter word:" << endl;
 	cin>>ws;
 	getline(cin,input,'\n');
-	
+
 	if (command2 == 1 || command2 == 4)
-		{ if (RedBlackTree.rbFindWord(input) == input)
+		{
+		    if(rbTreebuilt){
+		    if (RedBlackTree.rbFindWord(input) == input)
 			{
 				cout << "Operations Count: " << RedBlackTree.getOpCount() << endl;
 				cout << input << " found in Red-Black Tree" << endl;
 			}
 			RedBlackTree.resetOpCount();
-				
+
 			if (RedBlackTree.rbFindWord(input) == "nil")
 				{cout << "Operations Count: " << RedBlackTree.getOpCount() << endl;
 					cout << input << " not found in Red-Black Tree" << endl;
-					/*getSuggestions(input, &RedBlackTree, &Table, &LinkedList, command2);*/}}
+					/*getSuggestions(input, &RedBlackTree, &Table, &LinkedList, command2);*/}
+		    }
+		    else{
+                cout << "Red-Black Tree empty, please built or add to it first" << endl;
+		    }
+        }
 	if (command2 == 2 || command2 == 4)
-		{ if (Table.htFindWord(input) == input)
+		{
+		    if(hashBuilt){
+		    if (Table.htFindWord(input) == input)
 			{cout << "Operations Count: " << Table.getOpCount() << endl;
 				cout << input << " found in Hash Table" << endl;
 			}
 			Table.resetOpCount();
-				
+
 			if (Table.htFindWord(input) == "nil")
 				{cout << "Operations Count: " << Table.getOpCount() << endl;
-					cout << input << " not found in Hash Table" << endl;}}
+					cout << input << " not found in Hash Table" << endl;}
+            else{
+                cout << "Hash Table empty, please built or add to it first" << endl;
+		    }
+            }
+		}
 	if (command2 == 3 || command2 == 4)
-		{LinkedList.resetOpCount();
+		{
+		    if(listBuilt){
+		    LinkedList.resetOpCount();
 			if (LinkedList.llFindWord(input) == input)
 			{cout << "Operations Count: " << LinkedList.getOpCount() << endl;
 				cout << input << " found in Linked List" << endl;
 			}
 			LinkedList.resetOpCount();
-				
+
 			if (LinkedList.llFindWord(input) == "nil")
 				{cout << "Operations Count: " << LinkedList.getOpCount() << endl;
-					cout << input << " not found in Linked List" << endl;}}
+					cout << input << " not found in Linked List" << endl;}
+            else{
+                cout << "Linked List empty, please built or add to it first" << endl;
+		    }
+        }
+		}
 
 
 	break;
 }
 case 3: // add word to selected dictionary
-{	resetOpCounts(&RedBlackTree, &LinkedList, &Table);
+{
+    resetOpCounts(&RedBlackTree, &LinkedList, &Table);
 	int command2 = chooseDictionary();
 	if (command2 == 5)
 		{break;}
@@ -187,29 +219,31 @@ case 3: // add word to selected dictionary
 	cout << "Enter word:" << endl;
 	cin>>ws;
 	getline(cin,input,'\n');
-	
+
 	if (command2 == 1 || command2 == 4)
 	{
 		RedBlackTree.rbAddWord(input);
 		cout << '\n';
 		cout << input << " added to Red-Black Tree" << endl;
+		rbTreebuilt = true;
 	}
 	if (command2 == 2 || command2 == 4)
 	{
 		Table.htAddWord(input);
 		cout << '\n';
 		cout << input << " added to Hash Table" << endl;
+		hashBuilt = true;
 	}
 	if (command2 == 3 || command2 == 4)
 	{
 		LinkedList.llAddWordFast(input);
 		cout << '\n';
 		cout << input << " added to Linked List" << endl;
+		listBuilt = true;
 	}
 	break;
 }
-
-case 4:
+case 4: //edit a word
 {
 	int command2 = chooseDictionary();
 	if (command2 == 5)
@@ -224,11 +258,12 @@ case 4:
 
 
 
-	
+
 
 	resetOpCounts(&RedBlackTree, &LinkedList, &Table);
 	if (command2 == 1 || command2 == 4)
 	{
+	    if(rbTreebuilt){
 		if (RedBlackTree.rbFindWord(oldSpelling)!=oldSpelling)
 			{cout << oldSpelling << " not found in Red-Black Tree" << endl;
 		cout << "Operation cancelled" << endl;
@@ -241,9 +276,14 @@ case 4:
 		RedBlackTree.rbEditWord(oldSpelling,newSpelling);
 		cout << '\n';
 		cout << oldSpelling << " changed to " << newSpelling<< " in Red-Black Tree" << endl;
+	    }
+	    else{
+                cout << "Red-Black Tree empty, please built or add to it first" << endl;
+        }
 	}
 	if (command2 == 2 || command2 == 4)
 	{
+        if(hashBuilt){
 		if (Table.htFindWord(oldSpelling)!=oldSpelling)
 			{cout << oldSpelling << " not found in Hash Table" << endl;
 		cout << "Operation cancelled" << endl;
@@ -256,9 +296,14 @@ case 4:
 		Table.htEditWord(oldSpelling,newSpelling);
 		cout << '\n';
 		cout << oldSpelling << " changed to " << newSpelling << " in Hash Table" << endl;
+        }
+        else{
+                cout << "Red-Black Tree empty, please built or add to it first" << endl;
+        }
 	}
 	if (command2 == 3 || command2 == 4)
 	{
+	    if(listBuilt){
 		if (LinkedList.llFindWord(oldSpelling)!=oldSpelling)
 			{cout << oldSpelling << " not found in Linked List" << endl;
 		cout << "Operation cancelled" << endl;
@@ -272,6 +317,10 @@ case 4:
 		cout << '\n';
 
 		cout << oldSpelling << " changed to " << newSpelling << " in Linked List" << endl;
+	    }
+	    else{
+                cout << "Linked List empty, please built or add to it first" << endl;
+        }
 	}
 
 
@@ -279,7 +328,7 @@ case 4:
 
 	break;
 }
-case 5:
+case 5: //delete a word
 {
 	int command2 = chooseDictionary();
 	if (command2 == 5)
@@ -288,10 +337,11 @@ case 5:
 	cout << "Enter word to delete" << endl;
 	cin>>ws;
 	getline(cin,toDelete,'\n');
-	
+
 	resetOpCounts(&RedBlackTree, &LinkedList, &Table);
 	if (command2 == 1 || command2 == 4)
 	{
+        if(rbTreebuilt){
 		if (RedBlackTree.rbFindWord(toDelete)!=toDelete)
 			{cout << toDelete << " not found in Red-Black Tree" << endl;
 		cout << "Operation cancelled" << endl;
@@ -299,10 +349,14 @@ case 5:
 		RedBlackTree.rbDeleteWord(toDelete);
 		cout << "Operations Count: " << RedBlackTree.getOpCount() << endl;
 		cout << toDelete << " deleted from Red-Black Tree" << endl;
-
+        }
+        else{
+                cout << "Red-Black Tree empty, please built or add to it first" << endl;
+        }
 	}
 	if (command2 == 2 || command2 == 4)
 	{
+	    if(hashBuilt){
 		if (Table.htFindWord(toDelete)!=toDelete)
 			{cout << toDelete << " not found in Hash Table" << endl;
 		cout << "Operation cancelled" << endl;
@@ -310,9 +364,14 @@ case 5:
 		Table.htDeleteWord(toDelete);
 		cout << "Operations Count: " << Table.getOpCount() << endl;
 		cout << toDelete << " deleted from Hash Table" << endl;
+	    }
+	    else{
+                cout << "Hash Table empty, please built or add to it first" << endl;
+        }
 	}
 	if (command2 == 3 || command2 == 4)
 	{
+	    if(listBuilt){
 		if (LinkedList.llFindWord(toDelete)!=toDelete)
 			{cout << toDelete << " not found in Linked List" << endl;
 		cout << "Operation cancelled" << endl;
@@ -320,6 +379,10 @@ case 5:
 		LinkedList.llDeleteWord(toDelete);
 		cout << "Operations Count: " << LinkedList.getOpCount() << endl;
 		cout << toDelete << " deleted from Linked List" << endl;
+	    }
+	    else{
+                cout << "Linked List empty, please built or add to it first" << endl;
+        }
 	}
 }
 
